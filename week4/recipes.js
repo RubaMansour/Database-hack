e8b72e91576b6caec8188e244b055c7e84641352
RubaMansour <Rubamansour73@gmail.com>
2025-01-19 03:08:35 +0100

    embdded

---
+++ week4/recipes.js
@@ -0,0 +1,53 @@
+
+const { MongoClient } = require('mongodb');
+
+const uri =  "mongodb+srv://rubamansour73:Ruba%40mansour@cluster1.jvvgj.mo>
+
+
+;
+
+async function connectToMongoDB() {
+  try {
+    const client = new MongoClient(uri);
+    await client.connect();
+    console.log("Connected to MongoDB Atlas");
+
+    const db = client.db('recipesDb');
+    const recipesCollection = db.collection('recipe-collection');
+
+    const pancakeRecipe = {
+      name: "Pancake",
+      category: "Breakfast",
+      ingredients: [
+        "1 cup flour",
+        "1 tbsp sugar",
+        "1 tsp baking powder",
+        "1/2 tsp salt",
+        "1 egg",
+        "1 cup milk",
+        "2 tbsp melted butter",
+        "1 tsp vanilla extract",
+        "1 tsp vegetable oil"
+      ],
+       steps: [
+        "Mix dry ingredients in a large bowl.",
+        "Whisk wet ingredients in another bowl.",
+        "Combine both wet and dry ingredients until smooth.",
+        "Heat a non-stick pan with some oil.",
+        "Pour batter onto the pan and cook until bubbles form on top.",
+        "Flip and cook until golden brown.",
+        "Serve with syrup, fruits, or chocolate."
+      ]
+    }
+const result = await recipesCollection.insertOne(pancakeRecipe);
+    console.log("Pancake Recipe Added:", result.insertedId);
+
+
+await client.close();
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+}
+
+
+connectToMongoDB();
